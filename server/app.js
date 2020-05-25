5950c2cc00c48a34d815de71096442c34478f81f
Varun Mathur <varun.mathur@live.com>
2020-05-25 14:58:13 -0400

    frontend cleanup + routing (#4)
    - fixed bugs to do with ID accesses to client map in server side code
    - room/create/routing functionality in frontend
    - backend/frontend integration
    - general frontend code cleanup

--- server/app.js
+++ server/app.js
@@ -8,8 +8,6 @@ var socketIo = require("socket.io");
 const { v4: uuidv4 } = require('uuid');
 var cors = require('cors')
 
-
-
 // key: id, value: room dict
 rooms = {};
 
@@ -20,9 +18,13 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIo(server);
 
+io.on('connection', function (socket) {
+  console.log("socket connection");
+});
+
 // creates a room with its own namespace and defines socket communication channels over that namespace
 // TOOD: eventually, we want to abstract this logic out of this file because this will become clunky
-function create_room(id, rounds, timeout) {
+function create_room(id) {
 
   // create namespace
   const nsp = io.of(`/rooms/${id}`);
@@ -33,11 +35,11 @@ function create_room(id, rounds, timeout) {
     'id': id,
     'clients': {},
     'state': 'pending',
-    'rounds': rounds,
+    'rounds': 3,
     'curRound': 0,
     'cluemaster': [],
     'curCluemaster': 0,
-    'timeout': timeout,
+    'timeout': 60,
     'clueQueue': [],
     'currWord': ''
   }
@@ -45,18 +47,21 @@ function create_room(id, rounds, timeout) {
   nsp.on('connection', function (socket) {
     console.log(`socket connected to room ${id}`);
 
+    // send the full game state to the new client
+    socket.emit('state', rooms[id]);
+
     // add yourself to the client list and update all players
-    let room = rooms[id];
-    let client = { 'name': socket.id }; //changed name variable to socket.id
-    room['clients'][id] = client;
-    nsp.emit('clients', room['clients']); //emits list of players in the namespace
+    let clientMap = rooms[id]['clients'];
+    let client = { 'name': '' };
+    clientMap[socket.id] = client;
+    nsp.emit('clients', clientMap); //emits list of players in the namespace
 
     //When player edits name
     socket.on("edit_name", (args) => {
       //update rooms, replace name
       let newName = args['name'];
       let clientMap = rooms[id]['clients'];
-      clientMap[id]['name'] = newName;
+      clientMap[socket.id]['name'] = newName;
       nsp.emit('clients', clientMap); //emits list of players in the namespace
       console.log(`${id}:${socket.id} changed name to ${newName}`);
     });
@@ -128,20 +133,19 @@ app.get('/test', function (req, res) {
 });
 
 // Route for creating a new room
-app.post('/newroom', function (req, res) {
+app.get('/rooms/new', function (req, res) {
   let newRoomId = uuidv4();
-  console.log(req.body);
-  create_room(newRoomId, req.body.rounds, req.body.timeout);
+  create_room(newRoomId);
   res.send(newRoomId);
 });
 
-// Route for accessing a room id
-app.get('/rooms/:id', function (req, res, next) {
+// Route for accessing a room id -- returns 200 if room exists and 404 if not
+app.get('/rooms/exists/:id', function (req, res, next) {
   let id = req.params.id;
   if (id in rooms) {
-    res.send(`room ${id} exists`);
+    res.send("true");
   } else {
-    res.send(`room ${id} does not exist.`)
+    res.send("false");
   }
 });
 
